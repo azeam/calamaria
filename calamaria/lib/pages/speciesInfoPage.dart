40b4ec3df38acfb57cad634d32f0aaef06a35623
azeam <dennishagg@gmail.com>
2021-01-18 19:33:45 +0100

    fix broken pdf link

--- calamaria/lib/pages/speciesInfoPage.dart
+++ calamaria/lib/pages/speciesInfoPage.dart
@@ -12,24 +12,21 @@ class PageState_SpeciesInfoPage extends State<PageSpeciesInfo> {
   final int speciesId;
   List<Species> species;
   PageState_SpeciesInfoPage(this.speciesId);
-  String title = "";
+  String title = "Loading...";
 
   void initState() {
     super.initState();
     loadJson();
   }
 
-  // load everything on init and set state in order to get species name for title (+ species list to pass)
+  // load everything on init and set state in order to get species name for title + species list to pass as arg
   // instead of using multiple futurebuilders
   loadJson() async {
     String data = await rootBundle.loadString('assets/species.json');
     final parsed = json.decode(data).cast<Map<String, dynamic>>();
-    setState(() => {
-          species = parsed
-              .map<Species>((json) => new Species.fromJSON(json))
-              .toList(),
-          title = species[this.speciesId].scientificName.toString(),
-        });
+    species = parsed.map<Species>((json) => Species.fromJSON(json)).toList();
+    title = species[this.speciesId].scientificName.toString();
+    setState(() => {});
   }
 
   @override
